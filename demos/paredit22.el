0223efbbf9f123a9c3c7288ec2126846f981df8e
Roful <88769906+Roful@users.noreply.github.com>
2021-11-26 06:56:09 +0000

    Updated the openpmd-api feedstock.

---
+++ demos/paredit22.el
@@ -0,0 +1,12 @@
+;;; paredit.el --- minor mode for editing parentheses  -*- Mode: Emacs-Lisp -*-
+
+;; Copyright (C) 2005--2010 Taylor R. Campbell
+
+;; Author: Taylor R. Campbell
+;; Version: 22
+;; Created: 2005-07-31
+;; Keywords: lisp
+
+;; Paredit is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License
