913ca7fa491c1623f3863ec7420f97f275cf626e
Roful <88769906+Roful@users.noreply.github.com>
2023-02-24 18:54:56 +0000

    Added scripts.

--- utils.rkt
+++ utils.rkt
@@ -4,4 +4,35 @@
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#lang racket
+
+(provide (all-defined-out))
+
+
+
+;-------------------------------------------------------------
+;                        utilities
+;-------------------------------------------------------------
+
+(define-syntax letv
+  (syntax-rules ()
+    [(_ () body ...)
+     (begin body ...)]
+    [(_ ([(e1 e2* ...) e3] bd* ...) body ...)
+     (let-values ([(e1 e2* ...) e3])
+       (letv (bd* ...) body ...))]
+    [(_ ([e1 e2] bd* ...) body ...)
+     (let ([e1 e2])
+       (letv (bd* ...) body
